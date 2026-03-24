b4c8764b8322f9cab7050eaec8a8b0eb76246dee
Stuart Sears <stuart@sjsears.com>
2026-03-24 10:32:53 +0000

    use m as prefix for mini align commands

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,13 @@
 return {
   { "nvim-mini/mini.icons" },
   { "nvim-mini/mini.surround" },
+  {
+    "nvim-mini/mini.align",
+    opts = {
+      mappings = {
+        start = "ma",
+        start_with_preview = "mA",
+      },
+    },
+  },
 }
