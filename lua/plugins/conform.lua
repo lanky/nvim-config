14e12db7c9c8649033767fc6b6b3c759b3e88efa
Stuart Sears <stuart@sjsears.com>
2026-06-05 20:36:03 +0100

    use individual ruff formatters in conform.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,20 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = {"ruff"},
-        },
-        default_format_opts = {
-            lsp_format = "fallback",
-        },
-        format_on_save = {
-            lsp_format = "fallback",
-            timeout_ms = 500,
-        },
-    }
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  --@module "conform"
+  --@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "ruff_fix", "ruff_organize_imports", "ruff_format", lsp_format = "first" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = {
+      lsp_format = "fallback",
+      timeout_ms = 500,
+    },
+  },
 }
